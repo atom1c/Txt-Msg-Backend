1cf4b5f97534b9de6a1b235f27a754b25abf78eb
Jake <jake@eakjb.com>
2014-07-27 18:01:18 -0600

    Node.js test - this might be screwing stuff up

---
+++ Main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Jake on 7/27/2014.
+ */
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
